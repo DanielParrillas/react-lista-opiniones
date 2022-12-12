00a3cb61764340428bf79fdd710a4f1cea69a86e
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-12-12 07:52:47 -0600

    Create Opinion.jsx

---
+++ src/components/Opinion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Opinion () {
+  return (
+    <div className="container-opinion">
+      <img
+        className="image-opinion"
+        src=""
+      />
+    </div>
+  );
+}
