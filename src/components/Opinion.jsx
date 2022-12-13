4d1b54d8d70fad973289ffa880e71807f3c82c9f
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-12-13 10:03:16 -0600

    sctructure

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Opinion () {
+function Opinion() {
   return (
     <div className="container-opinion">
       <img
         className="image-opinion"
-        src=""
+        src={require("../images/testimonio-emma.png")}
+        alt={"Foto de emma"}
       />
     </div>
   );
